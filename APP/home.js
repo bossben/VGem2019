9f947dbde6bae5ef6a00272e00abd038fb522459
Benjamin Ascoli <ben.ascoli@gmail.com>
2019-08-02 13:34:32 -0400

    a

--- APP/home.js
+++ APP/home.js
@@ -49,6 +49,14 @@ $(document).ready(function(){
             setWidth();
         });
     }
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-90
+        }, 1200)
+    });
 });
 
 
